67a823a057abda84b46e9e12fe45ff3e7704c388
tranhungthoi21-png <tranhungthoi21@gmail.com>
2026-08-23 20:13:16 +0700

    Update CNNmod.scala

--- chipyard/chipyard/src/main/scala/CNNmod/CNNmod.scala
+++ chipyard/chipyard/src/main/scala/CNNmod/CNNmod.scala
@@ -7,38 +7,50 @@ import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.regmapper.{RegField, RegFieldDesc}
 import freechips.rocketchip.subsystem.BaseSubsystem
 import freechips.rocketchip.tilelink.{TLFragmenter, TLRegisterNode}
-import freechips.rocketchip.util.{ElaborationArtefacts, ClockGate}
+import freechips.rocketchip.util.ElaborationArtefacts
 import org.chipsalliance.cde.config._
 
-case class CNNParams(
+
+case class CNNParams  //dinh nghia tham so (dia chi va do rong cho IP)
+(
   Addr: BigInt = 0x06400000L,
   dataWidth: Int = 32
-)
+){}
 
 case object CNNKeys extends Field[Option[CNNParams]](None)
 
 class CNN()(implicit p: Parameters) extends BlackBox with HasBlackBoxResource {
-  override def desiredName = "CNN_System_Top"
+  override def desiredName = "CNN_Wrapper"
   val io = IO (new Bundle{
-    val clk            = Input(Clock())
-    val rst_n          = Input(Bool())
-    val start          = Input(Bool())
-    val config_mode    = Input(Bool())
-    val cpu_data_in    = Input(UInt(20.W))
-    val cpu_wr_en      = Input(Bool())
-    val ready_for_data = Output(Bool())
+    // Clock and reset
+    val clk = Input(Clock())
+    val rst_n = Input(Bool())
+
+    // Weight SRAM load path
+    val config_mode = Input(Bool())
+    val weight_valid = Input(Bool())
+    val weight_data = Input(UInt(20.W))
+
+    // ECG sample input
+    val ecg_valid = Input(Bool())
+    val ecg_data = Input(UInt(8.W))
+
+    // Control inference
+    val start_infer = Input(Bool())
+
+    // Outputs
     val classification = Output(UInt(3.W))
-    val done_all       = Output(Bool())
+    val done = Output(Bool())
   })
   addResource("/vsrc/CNN_System_Top.sv")
 }
 
-class CNNDevice(val params: CNNParams, beatBytes: Int = 8)(implicit p: Parameters)
+class CNNDevice(val params: CNNParams, beatBytes: Int = 8)(implicit p: Parameters)  //TileLink Peripheral
   extends LazyModule {
   val device = new SimpleDevice("CNNmod", Seq("SoC,CNN"))
   val mmioNode = TLRegisterNode(
-    address   = Seq(AddressSet(params.Addr, 4096-1)),
-    device    = device,
+    address     = Seq(AddressSet(params.Addr, 4096-1)),
+    device      = device,
     beatBytes = beatBytes,
     concurrency = 1
   )
@@ -52,90 +64,73 @@ class CNNDeviceImp(outer: CNNDevice)(implicit p: Parameters)
   val cnn = Module(new CNN())
 
   // ==== MMIO Registers ====
-  val r_reset       = RegInit(false.B)
-  val r_start       = RegInit(false.B)
-  val r_config_mode = RegInit(false.B)
-  val r_wr_en       = RegInit(false.B)
-  val r_data_in     = RegInit(0.U(20.W))
+  val r_reset        = RegInit(false.B)
+  val r_config_mode  = RegInit(false.B)
+  
+  val r_weight_data  = RegInit(0.U(20.W))
+  val r_ecg_data     = RegInit(0.U(8.W))
 
-  val r_ready          = RegInit(false.B)
+  // Các thanh ghi m?c luu giá tr? ghi t? MMIO
+  val r_weight_valid = RegInit(false.B)
+  val r_ecg_valid    = RegInit(false.B)
+  val r_start_infer  = RegInit(false.B)
+
+  // Dùng RegNext d? t?o xung edge-detection (su?n lên)
+  val weight_valid_dly  = RegNext(r_weight_valid, false.B)
+  val weight_valid_pulse = r_weight_valid && !weight_valid_dly
+
+  val ecg_valid_dly     = RegNext(r_ecg_valid, false.B)
+  val ecg_valid_pulse    = r_ecg_valid && !ecg_valid_dly
+
+  val start_infer_dly   = RegNext(r_start_infer, false.B)
+  val start_infer_pulse  = r_start_infer && !start_infer_dly
+
   val r_classification = RegInit(0.U(3.W))
   val r_done           = RegInit(false.B)
 
-  // T?o xung WREN t? su?n lên
-  val r_wr_en_dly = RegNext(r_wr_en, false.B)
-  val wr_en_pulse = r_wr_en && !r_wr_en_dly
-
   // ==========================================================
-  // CLOCK GATING: ÉP T?C Ð? RTL B?NG T?C Ð? CPU MMIO
+  // Connect Chipyard -> CNN Wrapper
   // ==========================================================
-  val cnn_clk_en = Wire(Bool())
-
-  when (r_start && !r_config_mode && cnn.io.ready_for_data) {
-    // Trong pha bom Data, clock CH? N?Y M?T NH?P khi CPU nh?p wr_en_pulse
-    cnn_clk_en := wr_en_pulse
-  } .otherwise {
-    // Các pha còn l?i (Config Weights, Inference, Reset) ch?y Clock liên t?c
-    cnn_clk_en := true.B
-  }
-
-  // ?? S? D?NG ClockGate() T? RocketChip Ð? T?O GATED CLOCK CHU?N
-  val cnn_gated_clk = ClockGate(clock, cnn_clk_en)
-
-  // ==========================================================
-  // Connect Chipyard -> CNN
-  // ==========================================================
-  cnn.io.clk         := cnn_gated_clk
+  cnn.io.clk         := clock
   cnn.io.rst_n       := !r_reset
-  cnn.io.start       := r_start
-  cnn.io.config_mode := r_config_mode
-  cnn.io.cpu_data_in := r_data_in
-  cnn.io.cpu_wr_en   := wr_en_pulse
+
+  cnn.io.config_mode  := r_config_mode
+  cnn.io.weight_valid := weight_valid_pulse
+  cnn.io.weight_data  := r_weight_data
+  cnn.io.ecg_valid    := ecg_valid_pulse
+  cnn.io.ecg_data     := r_ecg_data
+  cnn.io.start_infer  := start_infer_pulse
 
   // ==========================================================
-  // CNN -> Chipyard (Ch?t k?t qu? an toàn)
+  // CNN Wrapper -> Chipyard
   // ==========================================================
-  r_ready := cnn.io.ready_for_data
-
-  val r_start_dly = RegNext(r_start, false.B)
-  val start_pulse = r_start && !r_start_dly
-
-  when (r_reset || start_pulse) {
-    r_done           := false.B
-    r_classification := 0.U
-  } .elsewhen (cnn.io.done_all) {
-    r_done           := true.B
-    r_classification := cnn.io.classification
+  r_classification := cnn.io.classification
+  when (r_reset || start_infer_pulse) {
+    r_done             := false.B
+    r_classification   := 0.U
+  } .elsewhen (cnn.io.done) {
+    r_done             := true.B
+    r_classification   := cnn.io.classification
   }
 
-  // ==== MMIO Mapping ====
   outer.mmioNode.regmap(
-    CNNRegs.control   -> Seq(
-      RegField(1, r_reset,       RegFieldDesc("r_reset", "reset")),
-      RegField(1, r_start,       RegFieldDesc("start","Start CNN inference")),
-      RegField(1, r_config_mode, RegFieldDesc("config_mode", "CNN config mode")),
-      RegField(1, r_wr_en,       RegFieldDesc("cpu_wr_en", "Enable write SRAM")),
-      RegField(28)),
-    CNNRegs.data_in -> Seq(
-      RegField(20, r_data_in,    RegFieldDesc("data_in", "20-bit data input")),
-      RegField(12)),
-    CNNRegs.ready -> Seq(
-      RegField.r(1, r_ready, RegFieldDesc("ready", "CNN ready", volatile = true)),
-      RegField(31)),
-    CNNRegs.classification -> Seq(
-      RegField.r(3, r_classification, RegFieldDesc("classification", "CNN result", volatile = true)),
-      RegField(29)),
-    CNNRegs.done -> Seq(
-      RegField.r(1, r_done, RegFieldDesc("done", "CNN completed", volatile = true)),
-      RegField(31)
-    )
+    0x00 -> Seq(RegField(1, r_reset, RegFieldDesc("r_reset", "Reset CNN wrapper"))),
+    0x04 -> Seq(RegField(1, r_config_mode, RegFieldDesc("config_mode", "1 = Weight load mode, 0 = Inference mode"))),
+    0x08 -> Seq(RegField(1, r_weight_valid, RegFieldDesc("weight_valid", "Pulse to write weight data"))),
+    0x0C -> Seq(RegField(20, r_weight_data, RegFieldDesc("weight_data", "20-bit weight value"))),
+    0x10 -> Seq(RegField(1, r_ecg_valid, RegFieldDesc("ecg_valid", "Pulse to write ECG sample"))),
+    0x14 -> Seq(RegField(8, r_ecg_data, RegFieldDesc("ecg_data", "8-bit ECG sample data"))),
+    0x18 -> Seq(RegField(1, r_start_infer, RegFieldDesc("start_infer", "Pulse to begin inference"))),
+    0x1C -> Seq(RegField.r(3, r_classification, RegFieldDesc("classification", "CNN classification result", volatile = true))),
+    0x20 -> Seq(RegField.r(1, r_done, RegFieldDesc("done", "Inference done pulse", volatile = true)))
   )
 }
 
-trait CanHavePeripheryCNNmod { this: BaseSubsystem =>
+trait CanHavePeripheryCNNmod{ this: BaseSubsystem =>   // noi IP vao SoC
   private val portName = "cnn"
-  val CNNDeviceOpt: Option[CNNDevice] = p(CNNKeys).map { params =>
-    val cnnWrapper = LazyModule(new CNNDevice(params, pbus.beatBytes)(p))
+
+  val CNNDeviceOpt: Option[CNNDevice] = p(CNNKeys).map {params =>
+    val cnnWrapper = LazyModule( new CNNDevice(params, pbus.beatBytes)(p))
     pbus.coupleTo(s"cnn_mmio_at_${params.Addr.toString(16)}") {
       cnnWrapper.mmioNode := TLFragmenter(pbus) := _
     }
@@ -143,7 +138,6 @@ trait CanHavePeripheryCNNmod { this: BaseSubsystem =>
   }
 }
 
-class WithCNNmod (base: BigInt = 0x06400000L, width: Int = 32) extends Config((site, here, up) => {
+class WithCNNmod (base: BigInt = 0x06400000L, width: Int = 32) extends Config((site, here, up) => {  //call in config
   case CNNKeys => Some(CNNParams(Addr = base, dataWidth = width))
 })
-
