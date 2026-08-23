50a1c354f766028e0fab5d69918502ff79edac10
tranhungthoi21-png <tranhungthoi21@gmail.com>
2026-08-23 20:13:36 +0700

    Update CNNRegs.scala

--- chipyard/chipyard/src/main/scala/CNNmod/CNNRegs.scala
+++ chipyard/chipyard/src/main/scala/CNNmod/CNNRegs.scala
@@ -1,8 +1,13 @@
 package chipyard.CNNmod
+
 object CNNRegs {
-  val control         = 0x00
-  val data_in         = 0x04
-  val ready           = 0x08
-  val classification  = 0x0C
-  val done            = 0x10
+  val r_reset         = 0x00
+  val config_mode     = 0x04
+  val weight_valid    = 0x08
+  val weight_data     = 0x0C
+  val ecg_valid       = 0x10
+  val ecg_data        = 0x14
+  val start_infer     = 0x18
+  val classification  = 0x1C
+  val done            = 0x20
 }
