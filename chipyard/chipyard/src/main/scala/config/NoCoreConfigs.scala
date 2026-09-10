450d88748567c3c922223f6e3cf7359217169294
tranhungthoi21-png <tranhungthoi21@gmail.com>
2026-09-10 16:17:04 +0700

    Delete chipyard/chipyard/src/main/scala/config directory

--- chipyard/chipyard/src/main/scala/config/NoCoreConfigs.scala
+++
@@ -1,9 +0,0 @@
-package chipyard
-
-import org.chipsalliance.cde.config.{Config}
-
-// A empty config with no cores. Useful for testing
-class NoCoresConfig extends Config(
-  new chipyard.config.WithNoDebug ++
-  new chipyard.config.WithNoPLIC ++
-  new chipyard.config.AbstractConfig)
